5c804651ba0da01416f3c712ed00de518567f3cb
y-shissy <yuki.s.0125@gmail.com>
2024-11-22 23:56:21 +0900

    initial

---
+++ app.py
@@ -0,0 +1,66 @@
+from openai import OpenAI
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+# .envファイルのパスを指定して読み込む
+load_dotenv()
+
+
+st.title("ChatGPT by Streamlit") # タイトルの設定
+
+# api_key = st.text_input("Enter your OpenAI API Key:", type="password")
+
+client = OpenAI(
+    # api_key=api_key
+    api_key=os.getenv('OPENAI_API_KEY'),
+)
+
+# セッション内で使用するモデルが指定されていない場合のデフォルト値
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o-mini"
+
+# セッション内のメッセージが指定されていない場合のデフォルト値
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# セッション内でチャット履歴をクリアするかどうかの状態変数
+if "Clear" not in st.session_state:
+    st.session_state.Clear = False
+
+# 以前のメッセージを表示
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# ユーザーからの新しい入力を取得
+if prompt := st.chat_input("質問を入力してください"):
+    st.session_state.messages.append({"role": "user", "content":[{'type':'text','text':prompt},],},)
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty() # 一時的なプレースホルダーを作成
+        full_response = ""
+        # ChatGPTからのストリーミング応答を処理
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.content if response.choices[0].delta.content else ""
+            message_placeholder.markdown(full_response + "▌") # レスポンスの途中結果を表示
+        message_placeholder.markdown(full_response) # 最終レスポンスを表示
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    st.session_state.Clear = True # チャット履歴のクリアボタンを有効にする
+
+# チャット履歴をクリアするボタンが押されたら、メッセージをリセット
+if st.session_state.Clear:
+    if st.button('clear chat history'):
+        st.session_state.messages = [] # メッセージのリセット
+        full_response = ""
+        st.session_state.Clear = False # クリア状態をリセット
+        st.rerun() # 画面を更新
